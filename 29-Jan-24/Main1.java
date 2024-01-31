e7b70ed3450e62c1ca25b8a83dbd34e96e85e79a
Virinchi Balusani <106929531+virinchiHub@users.noreply.github.com>
2024-01-31 23:57:26 +0530

    Add files via upload

---
+++ 29-Jan-24/Main1.java
@@ -0,0 +1,31 @@
+package com.sreyas.day1;
+
+public class Main1 {
+	int no;
+	String name;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main1  e1 = new Main1();
+		
+		e1.no = 1;
+		e1.name = "Virinchi";
+		
+		Main1 e2 = new Main1();
+		
+		e2.no = 2;
+		e2.name = "Sham";
+		
+		System.out.println(e1.no);
+		System.out.println(e1.name);
+		
+		System.out.println("----------------------------");
+		
+		System.out.println(e2.no);
+		System.out.println(e2.name);
+		
+		
+
+	}
+
+}
---
+++ 29-Jan-24/Main2.java
@@ -0,0 +1,26 @@
+package com.sreyas.day1;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();
+		 
+		s1.no = 1;
+		s1.name = "Virinchi";
+		
+		Student s2 = new Student();
+		
+		s2.no = 2;
+		s2.name = "Venkat";
+		
+		System.out.println(s1.no);
+		System.out.println(s1.name);
+		
+		System.out.println("---------------------");
+		
+		System.out.println(s2.no);
+		System.out.println(s2.name);
+	}
+
+}
